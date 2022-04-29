206313c7a5824d29e8d8247d080cce93564509b4
iamelijah2016 <iamelijah2016@gmail.com>
2022-04-29 12:06:30 +0800

    1.6.2 super() method

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Overriding method
+# super() method
 
 class User:
     def __init__(self, username, password):
@@ -6,7 +6,6 @@ class User:
         self.password = password
 
     def display1(self):
-        print("Parent class/User")
         print("Username:", self.username)
         print("Password", self.password)
 
@@ -15,17 +14,13 @@ class Admin(User):
     # overriding the init of class User
     def __init__(self, username, password, code):
         self.code = code
-        User.__init__(self, username, password)
+        # super() returns an object of the class User
+        super().__init__(username, password)
 
     def display3(self):
-        print("Subclass/Admin")
-        print("Username:", self.username)
-        print("Password:", self.password)
+        super().display1()
         print("Code:", self.code)
 
 
-u_45 = User("pretty_jun25", "otherpassword287")
 a_1 = Admin("leslie2001", "password 1234", "2468")
-
-u_45.display1()
 a_1.display3()
