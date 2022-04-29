5432dcd6e32bed65b48fc5d97ec11af89362c2cf
iamelijah2016 <iamelijah2016@gmail.com>
2022-04-29 14:36:16 +0800

    2.4 Coding Exercise

--- main.py
+++ main.py
@@ -1,44 +1,11 @@
-class ClubMembers:
-    def __init__(self, name, birthday, age, favorite_food, goal):
-        self.name = name
-        self.birthday = birthday
-        self.age = age
-        self.favorite_food = favorite_food
-        self.goal = goal
+flavors = ["vanilla", "chocolate", "strawberry", "cookies n' cream", "bubblegum"]
+toppings = ["almonds", "banana slices", "chocolate syrup", "caramel syrup", "white chocolate chips"]
 
-    def display1(self):
-        print("Name:", self.name)
-        print("Birthday:", self.birthday)
-        print("Age:", self.age)
-        print("Favorite food:", self.favorite_food)
-        print("Goal:", self.goal)
+ice_cream = dict(zip(flavors, toppings))
+print(ice_cream)
 
+ice_cream["chocolate"] = "blueberries"
+# print(ice_cream)
 
-class ClubOfficers(ClubMembers):
-    def __init__(self, name, birthday, age, favorite_food, goal, position):
-        self.position = position
-        # # my solution using super() method
-        # super().__init__(name, birthday, age, favorite_food, goal)
-
-        # the solution
-        ClubMembers.__init__(self, name, birthday, age, favorite_food, goal)
-
-    def display2(self):
-        # # my solution using super() method
-        # super().display1()
-        # print("Position:", self.position)
-
-        # the solution
-        print("Name:", self.name)
-        print("Birthday:", self.birthday)
-        print("Age:", self.age)
-        print("Favorite food:", self.favorite_food)
-        print("Goal:", self.goal)
-        print("Position:", self.position)
-
-
-m_1 = ClubMembers("Tom", "January 16", "14", "Ice cream", "To be happy")
-o_4 = ClubOfficers("Vera", "June 22", "16", "Beef stroganoff", "To be the world's greatest chef", "Treasurer")
-
-m_1.display1()
-o_4.display2()
+ice_cream.update({"matcha": "pistachios", "ube": "mango slices"})
+print(ice_cream)
