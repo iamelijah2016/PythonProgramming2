fdd2cf7a0e39858cdc72dd15652cdd10d100ead6
iamelijah2016 <iamelijah2016@gmail.com>
2022-04-29 11:27:33 +0800

    1.6 Inheritance

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
-class Costumers:
-    def __init__(self, name, beverage, food, total):
-        self.name = name
-        self.beverage = beverage
-        self.food = food
-        self.total = total
+# Inheritance
 
-    greeting = "Welcome to Coffee Palace"
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def display1(self):
+        print("(parent class/User) Username:", self.username, "password:", self.password)
 
 
-c_1 = Costumers("Nate", "Espresso", "Pastrami on rye", 220)
-c_2 = Costumers("Elaine", "Strawberry frappuccino", "Tuna wrap", 270)
-c_3 = Costumers("Samirah", "Iced caffe latte", "Cinnamon roll", 225)
-c_4 = Costumers("Jerry", "Caramel macchiato", "Glazed doughnut", 230)
-c_5 = Costumers("Paz", "Iced tea", "Blueberry pancakes", 315)
+class Admin(User):
+    def display2(self):
+        print("(subclass/Admin) Username:", self.username, "password:", self.password)
 
-print(Costumers.greeting)
-print(c_2.name)
-print(c_2.beverage)
-print(c_2.food)
-print(c_2.total)
-print(c_4.name)
-print(c_4.beverage)
-print(c_4.food)
-print(c_4.total)
+
+a_1 = Admin("leslie2001", "password1234")
+u_45 = User("pretty_jun25", "otherpassword287")
+
+u_45.display1()
+a_1.display2()
