10536f2a4744d930cfcd96051fbd0ff1189829da
iamelijah2016 <iamelijah2016@gmail.com>
2022-04-29 11:53:55 +0800

    1.6.1 Overriding method

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Inheritance
+# Overriding method
 
 class User:
     def __init__(self, username, password):
@@ -6,16 +6,26 @@ class User:
         self.password = password
 
     def display1(self):
-        print("(parent class/User) Username:", self.username, "password:", self.password)
+        print("Parent class/User")
+        print("Username:", self.username)
+        print("Password", self.password)
 
 
 class Admin(User):
-    def display2(self):
-        print("(subclass/Admin) Username:", self.username, "password:", self.password)
+    # overriding the init of class User
+    def __init__(self, username, password, code):
+        self.code = code
+        User.__init__(self, username, password)
+
+    def display3(self):
+        print("Subclass/Admin")
+        print("Username:", self.username)
+        print("Password:", self.password)
+        print("Code:", self.code)
 
 
-a_1 = Admin("leslie2001", "password1234")
 u_45 = User("pretty_jun25", "otherpassword287")
+a_1 = Admin("leslie2001", "password 1234", "2468")
 
 u_45.display1()
-a_1.display2()
+a_1.display3()
