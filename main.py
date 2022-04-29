fd6649de9403a05664f372e0e7ae233fe7abd6cf
iamelijah2016 <iamelijah2016@gmail.com>
2022-04-29 11:10:05 +0800

    1.5 Coding Exercise

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
+class Costumers:
+    def __init__(self, name, beverage, food, total):
+        self.name = name
+        self.beverage = beverage
+        self.food = food
+        self.total = total
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    greeting = "Welcome to Coffee Palace"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+c_1 = Costumers("Nate", "Espresso", "Pastrami on rye", 220)
+c_2 = Costumers("Elaine", "Strawberry frappuccino", "Tuna wrap", 270)
+c_3 = Costumers("Samirah", "Iced caffe latte", "Cinnamon roll", 225)
+c_4 = Costumers("Jerry", "Caramel macchiato", "Glazed doughnut", 230)
+c_5 = Costumers("Paz", "Iced tea", "Blueberry pancakes", 315)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(Costumers.greeting)
+print(c_2.name)
+print(c_2.beverage)
+print(c_2.food)
+print(c_2.total)
+print(c_4.name)
+print(c_4.beverage)
+print(c_4.food)
+print(c_4.total)
