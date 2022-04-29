89c21e5aeae7b35825f7a458af9e31f226a4dce3
iamelijah2016 <iamelijah2016@gmail.com>
2022-04-29 18:36:36 +0800

    3.12.e Coding Exercise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-try:
-    print(12*6)
-except:
-    print("This operation can't be performed.")
-else:
-    print("This operation can be performed.")
+best_burger = "Burger King"
+number_2_burger = "McDonald's"
+
+
+assert best_burger == "McDonald's"
+# assert number_2_burger == "McDonald's"
+
